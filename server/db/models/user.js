8a30ed19e1a9fee630653bdbc3090e9889c6f809
Mike Ingber <mbingber@gmail.com>
2015-11-11 15:39:18 -0500

    initial commit

---
+++ server/db/models/user.js
@@ -0,0 +1,50 @@
+'use strict';
+var crypto = require('crypto');
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String
+    },
+    salt: {
+        type: String
+    }
+});
+
+// generateSalt, encryptPassword and the pre 'save' and 'correctPassword' operations
+// are all used for local authentication security.
+var generateSalt = function () {
+    return crypto.randomBytes(16).toString('base64');
+};
+
+var encryptPassword = function (plainText, salt) {
+    var hash = crypto.createHash('sha1');
+    hash.update(plainText);
+    hash.update(salt);
+    return hash.digest('hex');
+};
+
+schema.pre('save', function (next) {
+
+    if (this.isModified('password')) {
+        this.salt = this.constructor.generateSalt();
+        this.password = this.constructor.encryptPassword(this.password, this.salt);
+    }
+
+    next();
+
+});
+
+schema.statics.generateSalt = generateSalt;
+schema.statics.encryptPassword = encryptPassword;
+
+schema.method('correctPassword', function (candidatePassword) {
+    return encryptPassword(candidatePassword, this.salt) === this.password;
+});
+
+mongoose.model('User', schema);
